708d13ae10e84373a369e48c7c77b85e3d2d58ad
bike <xmm123xmm@163.com>
2020-05-09 14:34:52 +0800

    add user arrange

---
+++ src/page/components/Schedule/components/SchedulePlan/index.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import { Button, Card, } from 'antd';
+import styled, { withTheme } from 'styled-components';
+// import './index.scss';
+import {
+    FormOutlined,
+  } from '@ant-design/icons';
+const SchedulePlanStyled = styled.div`
+.addBtnDiv{
+    display:flex;
+    padding-top:32px;
+    height:15px;
+    font-size:16px;
+    font-weight:300;
+    color:rgba(255,255,255,1);
+}
+p{
+    margin-bottom: 0;
+}
+.addBtn{
+    margin-bottom: 20px;
+    width: 100px;
+    height: 64px;
+    border: 0;
+    box-shadow: none;
+    background: none;
+    img{
+        margin-right: 20px;
+    }
+}
+.ant-card{
+    background: none;
+}
+.cardBox{
+    margin: 0 auto;
+    display: flex;
+}
+.card{
+    width: 250px;
+    height: 200px;
+    margin-right: 10px;
+    border: 1px solid #2140d0;
+    box-shadow: inset 0px 0px 10px 10px rgba(33,64,208,0.25);
+}
+.card:hover{
+    box-shadow: none;
+    background:
+        linear-gradient( 
+            #1DD5E6 0%, 
+            #46AEF7 100%
+        );
+    p{
+        color: #061B57;
+    }
+    .anticon{
+        color: #061B57;
+    }
+}
+.ant-card-head{
+    height: 30px;
+    min-height: 30px;
+    border-bottom: none;
+}
+.ant-card-extra{
+    padding: 0;
+}
+.cardContent{
+    display: flex;
+}
+.scheduleMemberDiv p {
+    margin-left: 10px;
+    height: 40px;
+    line-height: 40px;
+    border-bottom: 1px dashed #252b7a;
+}
+.scheduleMemberDiv p:last-child {
+    border-bottom: none;
+}
+.ant-card-body{
+    padding: 0 20px; 
+}
+.ant-list {
+    width:1697px;
+}
+.dateDiv{
+    padding-right: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-right: 1px solid #454da8;
+}
+`
+class SchedulePlan extends Component {
+    render() {
+        const arr = [1,2,3,4,5,6,7]
+        const { theme } = this.props
+        return (
+            <SchedulePlanStyled>
+                <div className="addBtnDiv">
+                
+                    <div>本周计划 / 添加计划</div>
+                
+                </div>
+                <div className="cardBox">
+                {arr.map(()=><Card className="card" title="" extra={<a href="#"><FormOutlined /></a>}>
+                        <div className="cardContent">
+                            <div className="dateDiv">
+                                <p>&lt;周一&gt;</p>
+                                <p>04/18</p>
+                                <p>至</p>
+                                <p>04/19</p>
+                            </div>
+                            <div className="scheduleMemberDiv">
+                                <p>08:00-12:00 李工程</p>
+                                <p>12:00-18:00 李工程</p>
+                                <p>18:00-24:00 李工程</p>
+                                <p>24:00-08:00 李工程</p>
+                            </div>
+                        </div>
+                    </Card>)}
+                </div>
+            </SchedulePlanStyled>
+        )
+    }
+}
+
+export default SchedulePlan
---
+++ src/page/components/UserArrange/index.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react'
+import './index.scss';
+import { CheckOutlined,FileTextFilled } from '@ant-design/icons';
+import styled, { withTheme } from 'styled-components';
+const UserArrangeStyled = styled.div`
+.user-arrange-user{
+    width: 264px;
+    padding-left:59px;
+    margin-bottom:23px;
+    height:17px;
+    font-size:16px;
+    font-weight:400;
+    color:rgba(255,255,255,1);
+}
+.user-arrange-inner{
+    width: 264px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .user-arrange-icon{
+        position: relative;
+        width: 59px;
+        height: 59px;
+        background:linear-gradient(0deg,rgba(70,174,247,1) 0%,rgba(29,213,230,1) 100%);
+        border-radius:50%;
+        text-align: center;
+        font-size: 36px;
+        color:#05154C;
+        z-index:1;
+    
+    }
+    .user-arrange-line{
+        width: 141px;
+        border-top:2px solid #1fcee4;
+
+    }
+    &.-current{
+        .user-arrange-icon.-right{
+            background:linear-gradient(0deg,rgba(247,203,107,1) 0%,rgba(251,169,128,1) 100%);
+        }
+    }
+    &.-wait{
+        .user-arrange-icon.-left{
+            &:after{
+                content:'';
+                position: absolute;
+                width: 67px;
+                height: 67px;
+                left: -4px;
+                top: -4px;
+                z-index:0;
+                border-radius: 50%;
+                background-color: rgba(255,255,255,0.5);
+            }
+            background:linear-gradient(0deg,rgba(247,203,107,1) 0%,rgba(251,169,128,1) 100%);
+        }
+        .user-arrange-icon.-right{
+            border-color:rgba(255,255,255,0.5);
+            opacity: 0.5;
+            background:linear-gradient(0deg,rgba(247,203,107,1) 0%,rgba(251,169,128,1) 100%);
+        }
+        .user-arrange-line{
+            border-color:#525252;
+            border-style: dashed;
+        }
+    }
+    
+}
+`
+
+
+
+class UserArrange extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const { theme, language, settingLang, menuSearchDispatch, menuSearchState, history } = this.props;
+        return (
+            <UserArrangeStyled>
+                <div className="user-arrange-user">{this.props.user}</div>
+                <div className={`user-arrange-inner ${this.props.current?'-'+this.props.current:null}`}>
+            
+                    <div class="user-arrange-icon -left">
+                        <CheckOutlined />
+                    </div>
+                    <div className="user-arrange-line">
+
+                    </div>
+                    <div class="user-arrange-icon -right">
+                        <FileTextFilled />
+                    </div>
+                </div>
+
+            </UserArrangeStyled>
+           
+        )
+    }
+}
+
+export default UserArrange
